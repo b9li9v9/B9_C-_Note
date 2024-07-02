644edff43518db9832d58c86e512bd8e7c0734e2
b9li9v9 <b9li9v9@gmail.com>
2024-07-03 00:05:04 +0800

    s

---
+++ src/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static int Main(string[] args)
+    {
+        int first;
+        System.Console.Write("set your first num: ");
+        first = System.Console.Read();
+        System.Console.WriteLine((Char)first);
+        return 0;
+    }
+}
